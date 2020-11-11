cb8b9ee1f0c5b25d78ee17602d32457dec60d644
kielkow <matheuskiel@fiorifer.com.br>
2020-11-11 17:02:39 -0300

    Augmenting Type Definitions

--- auth/src/middlewares/current-user.ts
+++ auth/src/middlewares/current-user.ts
@@ -1,6 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
+interface UserPayload {
+    id: string;
+    email: string;
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            currentUser?: UserPayload;
+        }
+    }
+}
+
 export const currentUser = (
     req: Request, 
     res: Response, 
@@ -14,7 +27,7 @@ export const currentUser = (
         const payload = jwt.verify(
             req.session.jwt, 
             process.env.JWT_KEY!
-        );
+        ) as UserPayload;
 
         req.currentUser = payload;
     } catch (err) {}
