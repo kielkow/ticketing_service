db98a06d0b2872d07b8ffdec5fa90fe23da11112
kielkow <matheuskiel@fiorifer.com.br>
2020-11-13 15:52:18 -0300

    Test invalid inputs

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,39 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('returns a 400 with an invalid e-mail', async () => {
+    return request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'invalid_email',
+        password: 'password'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+    return request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'test@test.com',
+        password: '123'
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing e-mail and password', async () => {
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'test@test.com',
+    })
+    .expect(400);
+    
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        password: '123',
+    })
+    .expect(400);
+});
