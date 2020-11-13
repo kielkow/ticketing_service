124233d89703e99c934f0c92adc5c24f1bb2fb6e
kielkow <matheuskiel@fiorifer.com.br>
2020-11-13 16:14:54 -0300

    Requiring Unique E-mails

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -46,3 +46,21 @@ it('returns a 400 with missing e-mail and password', async () => {
     })
     .expect(400);
 });
+
+it('disallows duplicate e-mails', async () => {
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'test@test.com',
+        password: 'password'
+    })
+    .expect(201);
+    
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        email: 'test@test.com',
+        password: 'password'
+    })
+    .expect(400);
+});
