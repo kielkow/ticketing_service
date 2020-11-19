ab0680a7e97571a5caf1c53f86b69e4d6adf92c3
kielkow <matheuskiel@fiorifer.com.br>
2020-11-19 15:21:40 -0300

    An onSuccess callback

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useState } from 'react';
 
-export default ({ url, method, body }) => {
+export default ({ url, method, body, onSuccess }) => {
     const [errors, setErrors] = useState(null);
 
     const doRequest = async () => {
@@ -9,6 +9,10 @@ export default ({ url, method, body }) => {
             setErrors(null);
 
             const response = await axios[method](url, body);
+
+            if (onSuccess) {
+                onSuccess(response.data);
+            }
 
             return response.data;
         } 
