1e24cfe714d167983d0872bacf327bf8f897e092
kielkow <matheuskiel@fiorifer.com.br>
2020-11-20 16:42:50 -0300

    Specifying the host

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-    // console.log(currentUser);
+    console.log(currentUser);
 
     // axios.get('/api/users/currentuser');
 
@@ -10,10 +10,21 @@ const LandingPage = ({ currentUser }) => {
 
 LandingPage.getInitialProps = async () => {
     if (typeof window === 'undefined') {
+        const { data } = await axios.get(
+            'http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser',
+            {
+                headers: {
+                    Host: 'ticketing.dev'
+                }
+            }
+        );
 
+        return data;
     }
     else {
+        const { data } = await axios.get('/api/users/currentuser');
 
+        return data;
     }
 
     return {};
