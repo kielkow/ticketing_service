682978cdc70465e3e851f744d1a8b23435cdb66f
kielkow <matheuskiel@fiorifer.com.br>
2020-11-20 19:18:21 -0300

    A reusable api client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
     console.log(currentUser);
@@ -6,22 +6,12 @@ const LandingPage = ({ currentUser }) => {
     return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-    if (typeof window === 'undefined') {
-        const { data } = await axios.get(
-            'http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser',
-            {
-                headers: req.headers
-            }
-        );
+LandingPage.getInitialProps = async context => {
+    const client = buildClient(context);
 
-        return data;
-    }
-    else {
-        const { data } = await axios.get('/api/users/currentuser');
+    const { data } = await client.get('/api/users/currentuser');
 
-        return data;
-    }
+    return data;
 };
 
 export default LandingPage;
