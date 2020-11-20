eb10d15610356992ece37034c538e4eb7ba9f1b4
kielkow <matheuskiel@fiorifer.com.br>
2020-11-20 15:22:20 -0300

    GetInital props called

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,17 +1,21 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-    console.log(currentUser);
+    // console.log(currentUser);
 
-    // axios.get('/api/users/currentuser');
+    // // axios.get('/api/users/currentuser');
 
     return <h1>Landing Page</h1>;
 };
 
 LandingPage.getInitialProps = async () => {
-    const response = await axios.get('/api/users/currentuser');
+    // const response = await axios.get('/api/users/currentuser');
 
-    return response.data;
+    // return response.data;
+
+    console.log('I WAS EXECUTED');
+
+    return {};
 };
 
 export default LandingPage;
